9b6efbba0be5065a020375d477f9b61b3c0a6df7
Alex <alex@192.168.1.11>
2021-03-13 21:27:32 -0600

    url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Arena",
+    title: `Arena Analytics`,
+    siteUrl: `https://www.arena-analytics.com/#about`,
+    description: `Blazing fast modern site generator for React`,
   },
   plugins: ['gatsby-plugin-postcss'],
 };
