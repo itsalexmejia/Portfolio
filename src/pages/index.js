78c8e55bc6a5dd1a890e7cb6065aa303dcf8627d
arena_analytics <arenaanalyticspage@gmail.com>
2021-03-15 19:41:28 -0600

    rc

--- src/pages/index.js
+++ src/pages/index.js
@@ -277,7 +277,7 @@ const IndexPage = ({data}) => {
                   </div>
                   <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                     <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                      <span className="block text-white">Consultoría especializada </span>
+                      <span className="block text-white">Consultoría especializada RC</span>
                       <span className="block text-indigo-200">en innovación y soluciones</span>
                     </h1>
                     <p className="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">Aliados de negocio de nuestros clientes en el desarrollo e implementación de soluciones que ayuden a mejorar y eficientar procesos con el mejor talento disponible, incrementando la rentabilidad.</p>
