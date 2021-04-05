1a6d5ac198128fad6dfe481cafcc69a9f40d76e6
arena_analytics <arenaanalyticspage@gmail.com>
2021-04-05 17:26:28 -0500

    banner color change

--- src/components/content/alert.js
+++ src/components/content/alert.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 // markup
 const AlertHome = props => {
   return (
-    <div className="relative bg-brand-600">
+    <div className="relative bg-bluearena-800">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div className="pr-16 sm:text-center sm:px-16">
           <p className="font-medium text-white">
