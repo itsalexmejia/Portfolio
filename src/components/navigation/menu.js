ece45e09c5779cbd408f9ff4dc069517a08b6cae
arena_analytics <arenaanalyticspage@gmail.com>
2021-06-08 00:15:09 -0500

    final changes

--- src/components/navigation/menu.js
+++ src/components/navigation/menu.js
@@ -24,7 +24,7 @@ const MenuTop = ({data}) => {
               <div className="relative z-20 shadow">
                   <div className="container-iphonex max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
                       <div>
-                          <Link to="/" className="flex">
+                          <Link to="/" className="flex" onMouseEnter={() => setOpenMoreDesk(false) & setOpenServiciosDesk(false)}>
                               <span className="sr-only">Arena Analytics</span>
                               <img className="h-8 w-auto sm:h-10" src={logoArena} alt="" />
                           </Link>
@@ -42,17 +42,17 @@ const MenuTop = ({data}) => {
                           <nav className="flex space-x-10">
                             
                                 
-                                <Link to="/" className="px-2 text-base font-medium text-gray-500 hover:text-gray-900">Inicio</Link>
+                                <Link to="/" className="px-2 text-base font-medium text-gray-500 hover:text-gray-900" onMouseEnter={() => setOpenMoreDesk(false) & setOpenServiciosDesk(false)}>Inicio</Link>
                               
-                                <div className="relative">
+                                <div className="relative bg-white" onMouseEnter={() => setOpenServiciosDesk(!openServiciosDesk) & setOpenMoreDesk(false)}>
                                   {/* Item active: "text-gray-900", Item inactive: "text-gray-500" */}
-                                  <button type="button" onMouseOver={() => setOpenServiciosDesk(!openServiciosDesk) & setOpenMoreDesk(false)} className="group bg-white rounded-lg text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-arena-500">
+                                  <button type="button" className="group bg-white rounded-lg text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-arena-500">
                                       <span>Servicios</span>
                                       {/*
                                         Heroicon name: solid/chevron-down
                                         Item active: "text-gray-600", Item inactive: "text-gray-400"
                                     */}
-                                      <svg className="ml-2 h-5 w-5 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                      <svg className="pl-2 h-5 w-7 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                           <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
                                       </svg>
                                   </button>
@@ -60,11 +60,11 @@ const MenuTop = ({data}) => {
 
                               {/* <Link to="/" className="text-base font-medium text-gray-500 hover:text-gray-900">Industrias</Link> */}
                               {/* <Link to="/" className="text-base font-medium text-gray-500 hover:text-gray-900">Insights</Link> */}
-                              <Link to="/careers/" className="px-2 text-base font-medium text-gray-500 hover:text-gray-900">Careers</Link>
+                              <Link to="/careers/" className="px-2 text-base font-medium text-gray-500 hover:text-gray-900" onMouseEnter={() => setOpenMoreDesk(false) & setOpenServiciosDesk(false)}>Careers</Link>
 
-                              <div className="relative">
+                              <div className="relative bg-white" onMouseEnter={() => setOpenMoreDesk(!openMoreDesk) & setOpenServiciosDesk(false)}>
                                   {/* Item active: "text-gray-900", Item inactive: "text-gray-500" */}
-                                  <button type="button" onMouseOver={() => setOpenMoreDesk(!openMoreDesk) & setOpenServiciosDesk(false)} className="group bg-white rounded-lg text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-arena-500">
+                                  <button type="button" className="group bg-white rounded-lg text-gray-500 inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-arena-500">
                                       <span>Nosotros</span>
                                       {/*
                                         Heroicon name: solid/chevron-down
@@ -76,10 +76,10 @@ const MenuTop = ({data}) => {
                                   </button>
                               </div>
                               {/* activeClassName="bg-arena-50 rounded-xl text-arena-600 font-semibold" */}
-                              <Link to="/nosotros/responsabilidad/" className="hidden px-2 text-base font-medium xl:block text-gray-500 hover:text-gray-900">Responsabilidad Social</Link>
+                              <Link to="/nosotros/responsabilidad/" className="hidden px-2 text-base font-medium xl:block text-gray-500 hover:text-gray-900" onMouseEnter={() => setOpenMoreDesk(false) & setOpenServiciosDesk(false)}>Responsabilidad Social</Link>
                           
                           </nav>
-                          <div className="flex items-center md:ml-12">
+                          <div className="flex items-center md:ml-12" onMouseEnter={() => setOpenMoreDesk(false) & setOpenServiciosDesk(false)}>
                               <Link to="/contacto/" className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-lg shadow-sm text-base font-medium text-white bg-arena-600 hover:bg-brand-700 active:bg-brand-400" activeClassName="bg-gray-400">Contáctanos</Link>
                           </div>
                       </div>
